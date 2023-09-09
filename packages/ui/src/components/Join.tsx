228e202de9593a965e4039c7bbabf1d93c1fcffa
Siddhant <git@sdnts.dev>
2023-09-08 19:44:24 -0500

    improve(ui): use toasts on the `/tunnel/join` page

--- packages/ui/src/components/Join.tsx
+++ packages/ui/src/components/Join.tsx
@@ -1,18 +1,21 @@
-import { Fragment, useEffect, useRef, useState } from "react";
-import { formatSize, useStore } from "../store";
 import clsx from "clsx";
 import { animate, timeline } from "motion";
+import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
+import { useStore } from "../store";
+import { ErrorToast } from "./Toasts";
 
 export const Join = () => {
-  const state = useStore((s) => s.state);
-  const setState = useStore((s) => s.setState);
-  const blobs = useStore((s) => s.blobs);
+  const [state, setState] = useStore((s) => [s.state, s.setState]);
 
   const [secret, setSecret] = useState("");
   const secretInput = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (secret === null || secret.length !== 6) return;
+
+    const t = toast.loading("Joining tunnel", { duration: 5000 });
+    sessionStorage.clear();
 
     fetch(
       `http${import.meta.env.PROD ? "s" : ""}://${import.meta.env.PUBLIC_API_HOST
@@ -23,14 +26,25 @@ export const Join = () => {
         if (res.status === 200) return res.json();
         return Promise.reject();
       })
-      .then((res: { auth: string }) => {
-        sessionStorage.setItem("auth", res.auth);
+      .then((res: { token: string }) => {
+        if (!res.token) throw new Error("Malformed API response");
+
+        toast.success("Ready", { id: t, duration: 10_000 });
+
+        sessionStorage.setItem("token", res.token);
         sessionStorage.setItem("peerId", "2");
 
         setState("ready");
         location.pathname = "/tunnel";
       })
       .catch(() => {
+        toast.error(
+          <ErrorToast>
+            Could not join tunnel, please try again in a bit!
+          </ErrorToast>,
+          { id: t, duration: 5000 }
+        );
+
         secretInput.current?.focus();
         animate(secretInput.current!, { outlineColor: "#F52F2F" });
         timeline(
@@ -77,35 +91,6 @@ export const Join = () => {
           }
         />
       </section>
-
-      {state === "ready" && blobs.length === 0 && (
-        <>
-          <span className="text-black text-2xl">Waiting for files</span>
-          <span className="text-gray text-xl">Drop files on the other end</span>
-        </>
-      )}
-
-      {state === "ready" && (
-        <section id="files" className="flex flex-col items-center">
-          {blobs.length > 0 && (
-            <ul className={clsx("mt-24 text-2xl", "grid grid-cols-2 gap-4")}>
-              {blobs.map((f) => (
-                <Fragment key={f.name}>
-                  <li className="text-right">
-                    <a
-                      href={`//${import.meta.env.PUBLIC_API_HOST}/download?id=${f.id
-                        }`}
-                    >
-                      <span className="text-right">{f.name}</span>
-                    </a>
-                  </li>
-                  <span className="text-gray">{formatSize(f.size)}</span>
-                </Fragment>
-              ))}
-            </ul>
-          )}
-        </section>
-      )}
     </main>
   );
 };
